849b6920f764720cf28120a1a332fe676c59a662
Serhat Say <34214176+serhat-say@users.noreply.github.com>
2021-04-11 14:58:23 +0300

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import nltk
+import re
+import pickle
+nltk.download(['punkt', 'wordnet'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
+
+def load_data(filename):
+    # Load data from db
+    engine = create_engine('sqlite:///' +filename)
+    df = pd.read_sql('select * from messages', engine)
+    X = df['message']
+    y = df.iloc[:, 4:39]
+    y['related'].replace(2, 1, inplace=True)
+    category_names = y.columns.tolist()
+    return X, y, category_names
+
+def tokenize(text):
+    text = re.sub(r'[^\w\s]','',text)
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    pipeline = Pipeline([
+            ('vect', CountVectorizer(tokenizer=tokenize)),
+            ('tfidf', TfidfTransformer()),
+            ('clf', MultiOutputClassifier(AdaBoostClassifier()))
+        ])
+    parameters = {
+            'vect__ngram_range': ((1, 1), (1, 2)),
+            'vect__max_df': (0.5, 0.75, 1.0),
+            'tfidf__use_idf': (True, False)
+        }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=2, n_jobs=-1)
+    return cv
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    y_pred_df = pd.DataFrame(y_pred, columns=category_names)
+    evaluation = {}
+    for column in Y_test.columns:
+        evaluation[column] = []
+        evaluation[column].append(precision_score(Y_test[column], y_pred_df[column]))
+        evaluation[column].append(recall_score(Y_test[column], y_pred_df[column]))
+        evaluation[column].append(f1_score(Y_test[column], y_pred_df[column]))
+    print(pd.DataFrame(evaluation))
+
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+        filename, model_path = sys.argv[1:]
+        X, Y, category_names = load_data(filename)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        model = build_model()
+        model.fit(X_train, Y_train)
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        save_model(model, model_path)
+
+
+if __name__ == '__main__':
+    main()
+
